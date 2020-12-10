6a090418ff6f4a6146daa50c99d2c756a55aa519
Ivan Ushatsky <ivan.ushatsky@gmail.com>
2020-12-10 13:51:34 +0300

    docs: Верстка todo-canvas-app

--- src/App.tsx
+++ src/App.tsx
@@ -1,103 +1,63 @@
-import React, {
-  FC,
-  memo,
-  useReducer,
-  useState,
-  useRef,
-  useEffect,
-} from "react";
-import {
-  createSmartappDebugger,
-  createAssistant,
-  AssistantAppState,
-} from "@sberdevices/assistant-client";
-import "./App.css";
-
-import { reducer } from "./store";
-
-const initializeAssistant = (getState: any) => {
-  if (process.env.NODE_ENV === "development") {
-    return createSmartappDebugger({
-      token: process.env.REACT_APP_TOKEN ?? "",
-      initPhrase: `Запусти ${process.env.REACT_APP_SMARTAPP}`,
-      getState,
-    });
-  }
-
-  return createAssistant({ getState });
-};
+import React, { FC, memo, useState} from 'react';
+import './App.css';
 
 export const App: FC = memo(() => {
-  const [appState, dispatch] = useReducer(reducer, { notes: [] });
+    const [notes, setNotes] = useState([{ id: 'uinmh', title: 'купить хлеб', completed: false }]);
+    const [note, setNote] = useState('');
 
-  const [note, setNote] = useState("");
-
-  const assistantStateRef = useRef<AssistantAppState>();
-  const assistantRef = useRef<ReturnType<typeof createAssistant>>();
-
-  useEffect(() => {
-    assistantRef.current = initializeAssistant(() => assistantStateRef.current);
-
-    assistantRef.current.on("data", ({ action }: any) => {
-      if (action) {
-        dispatch(action);
-      }
-    });
-  }, []);
-
-  useEffect(() => {
-    assistantStateRef.current = {
-      item_selector: {
-        items: appState.notes.map(({ id, title }, index) => ({
-          number: index + 1,
-          id,
-          title,
-        })),
-      },
+    const addNote = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        setNotes([...notes, { id: Math.random().toString(36).substring(7), title: note, completed: false }]);
+        setNote('');
     };
-  }, [appState]);
 
-  return (
-    <main className="container">
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
-          dispatch({ type: "add_note", note });
-          setNote("");
-        }}
-      >
-        <input
-          className="add-note"
-          type="text"
-          placeholder="Add Note"
-          value={note}
-          onChange={({ target: { value } }) => setNote(value)}
-          required
-          autoFocus
-        />
-      </form>
-      <ul className="notes">
-        {appState.notes.map((note, index) => (
-          <li className="note" key={note.id}>
-            <span>
-              <span style={{ fontWeight: "bold" }}>{index + 1}. </span>
-              <span
-                style={{
-                  textDecorationLine: note.completed ? "line-through" : "none",
-                }}
-              >
-                {note.title}
-              </span>
-            </span>
-            <input
-              className="done-note"
-              type="checkbox"
-              checked={note.completed}
-              onChange={() => dispatch({ type: "done_note", id: note.id })}
-            />
-          </li>
-        ))}
-      </ul>
-    </main>
-  );
+    const deleteNote = (id: string) => {
+        setNotes(notes.filter(n => n.id !== id));
+    } 
+
+    const doneNote = (id: string) => {
+        setNotes(notes.map((n) => (n.id === id ? { ...n, completed: true } : n)))
+    };
+
+    return (
+        <main className="container">
+            <form
+                onSubmit={addNote}
+            >
+                <input
+                    className="add-note"
+                    type="text"
+                    placeholder="Add Note"
+                    value={note}
+                    onChange={({ target: { value } }) => setNote(value)}
+                    required
+                    autoFocus
+                />
+            </form>
+            <ul className="notes">
+                {notes.map((note, index) => (
+                    <li className="note" key={note.id}>
+                        <span>
+                            <span style={{ fontWeight: 'bold' }}>{index + 1}. </span>
+                            <span
+                                style={{
+                                    textDecorationLine: note.completed ? 'line-through' : 'none',
+                                }}
+                            >
+                                {note.title}
+                            </span>
+                        </span>
+                        <input
+                            className="done-note"
+                            type="checkbox"
+                            checked={note.completed}
+                            onChange={() => doneNote(note.id)}
+                            disabled={note.completed}
+                        />
+                        <div className="remove-note" onClick={() => deleteNote(note.id)}>X</div>
+                    </li>
+                ))}
+            </ul>
+        </main>
+    );
 });
